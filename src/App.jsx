60ea9bd40c413cb37335c2d5681021aaca79866e
Seyoung Kim <stothey0804@gmail.com>
2025-12-31 13:48:02 +0900

    feat: CardComponent 포지션 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,42 @@ import { Canvas, useLoader } from "@react-three/fiber";
 
 import * as THREE from "three";
 
-const CardComponent = ({ imageUrl }) => {
+const cardData = [
+  {
+    imageUrl: "/tarrot_1.png",
+    position: [0, -1, 0],
+    rotationY: -10,
+    rotationZ: 0,
+  },
+  {
+    imageUrl: "/tarrot_2.png",
+    position: [0.5, -1.05, -0.1],
+    rotationY: -10,
+    rotationZ: -10,
+  },
+  {
+    imageUrl: "/tarrot_3.png",
+    position: [-0.5, -1.05, 0.1],
+    rotationY: -10,
+    rotationZ: 10,
+  },
+  {
+    imageUrl: "/tarrot_4.png",
+    position: [-1, -1.15, 0.2],
+    rotationY: -10,
+    rotationZ: 20,
+  },
+  {
+    imageUrl: "/tarrot_5.png",
+    position: [1, -1.15, -0.1],
+    rotationY: -10,
+    rotationZ: -20,
+  },
+];
+
+const CardComponent = ({ position, rotationY, rotationZ, imageUrl }) => {
   const texture = useLoader(THREE.TextureLoader, imageUrl);
-  texture.colorSpace = THREE.DisplayP3ColorSpace;
+  texture.colorSpace = THREE.SRGBColorSpace;
 
   const materials = [
     new THREE.MeshStandardMaterial(), // 오른쪽
@@ -18,8 +51,14 @@ const CardComponent = ({ imageUrl }) => {
     }), // 앞면 (이미지 적용)
     new THREE.MeshStandardMaterial(), // 뒷면
   ];
+
   return (
-    <mesh material={materials}>
+    <mesh
+      position={position}
+      rotation-y={THREE.MathUtils.degToRad(rotationY)}
+      rotation-z={THREE.MathUtils.degToRad(rotationZ)}
+      material={materials}
+    >
       <boxGeometry args={[1, 1.6, 0.01]} />
     </mesh>
   );
@@ -31,8 +70,10 @@ function App() {
       <Canvas>
         <OrbitControls />
         <ambientLight intensity={0.7} />
-        <directionalLight intensity={2} position={[1, 1, 1]} />
-        <CardComponent imageUrl={"/card_1.png"} />
+        <directionalLight intensity={4} position={[4, 1, 4]} />
+        {cardData.map((props) => (
+          <CardComponent key={props.imageUrl} {...props} />
+        ))}
       </Canvas>
     </>
   );
